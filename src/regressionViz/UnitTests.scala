0868bb8f2c57786b47e623308d60c6b8afe8efb6
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-03-04 14:41:40 +0200

    The Data object returns the correct points

--- src/regressionViz/UnitTests.scala
+++ src/regressionViz/UnitTests.scala
@@ -30,10 +30,27 @@ class UnitTests {
     assertTrue("A dataset with a name data points should return Some(name)", someData.name == Some("dataset's name"))    
   }
   
+  @Test def someDataPointsExists () {
+    val someData = new Data(points = Some(Array(Array(1.23, 3.1343, 2), Array(0, 1, 2))))
+    assertTrue("A dataset with data some points should return points.isDefined == true", someData.points.isDefined == true)    
+  }
+  
   @Test def someDataPoints () {
     val someData = new Data(points = Some(Array(Array(1.23, 3.1343, 2), Array(0, 1, 2))))
-    assertTrue("A dataset with data some points should return Some(points)", someData.points == Some(Array(Array(1.23, 3.1343, 2), Array(0, 1, 2))))    
-  }
+//    for{
+//      something <- someData.points
+//      array <- something
+//      point <- array
+//    } println(point)
+    assertTrue("A dataset with data some points should return the points", (
+        someData.points.get(0)(0) == 1.23 &&
+        someData.points.get(0)(1) == 3.1343 &&
+        someData.points.get(0)(2) == 2 &&
+        someData.points.get(1)(0) == 0 &&
+        someData.points.get(1)(1) == 1 &&
+        someData.points.get(1)(2) == 2) )    
+  }  
+ 
   
   // create a dataset with unequal number of values  
   
