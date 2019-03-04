a0e025c17de21c2e1c922ff3c3d215f54f732db0
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-03-04 14:23:09 +0200

    points from data are not returning correctly

--- src/regressionViz/Data.scala
+++ src/regressionViz/Data.scala
@@ -2,7 +2,11 @@ package regressionViz
 
 import scala.collection.mutable.ArrayBuffer
 
-case class Data(points: Option[Array[Array[Double]]] = None, name: Option[String] = None, varNames: Option[ArrayBuffer[String]] = None) {
+case class Data(points: Option[Array[Array[Double]]] = None, 
+                name: Option[String] = None, 
+                varNames: Option[ArrayBuffer[String]] = None) 
+{
+
   
   
 }
--- src/regressionViz/UnitTests.scala
+++ src/regressionViz/UnitTests.scala
@@ -8,24 +8,34 @@ import org.junit.runner.RunWith
 class UnitTests {
   
   // Tests for an empty dataset
-  @Test def nonePoints () {
+  @Test def noneDataPoints () {
     val emptyData = new Data()
     assertTrue("An empty dataset's points should return None", emptyData.points == None)
  
   }
 
-  @Test def noneName () {
+  @Test def noneDataName () {
     val emptyData = new Data()
     assertTrue("A dataset without a name should return None", emptyData.name == None)    
   }
     
-  @Test def noneVarNames () {
+  @Test def noneDataVarNames () {
     val emptyData = new Data()
-    assertTrue("A dataset without data points should return None", emptyData.varNames == None)    
+    assertTrue("A dataset without variable names should return None", emptyData.varNames == None)    
   }
-      
+ 
+  // tests with something inside a dataset
+  @Test def someDataName () {
+    val someData = new Data(name = Some("dataset's name"))
+    assertTrue("A dataset with a name data points should return Some(name)", someData.name == Some("dataset's name"))    
+  }
   
-  // create a dataset with unequal number of values
+  @Test def someDataPoints () {
+    val someData = new Data(points = Some(Array(Array(1.23, 3.1343, 2), Array(0, 1, 2))))
+    assertTrue("A dataset with data some points should return Some(points)", someData.points == Some(Array(Array(1.23, 3.1343, 2), Array(0, 1, 2))))    
+  }
+  
+  // create a dataset with unequal number of values  
   
   // create another dataset with unequal number of values
   
