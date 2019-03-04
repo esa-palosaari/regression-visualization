0d3525cfe2c7eee516f397d716cf124761c9dae6
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-03-04 13:52:37 +0200

    first data tests

--- src/regressionViz/UnitTests.scala
+++ src/regressionViz/UnitTests.scala
@@ -2,7 +2,32 @@ package regressionViz
 
 import org.junit.Test
 import org.junit.Assert._
+import org.junit.runner.RunWith
+
 
 class UnitTests {
   
+  // Tests for an empty dataset
+  @Test def nonePoints () {
+    val emptyData = new Data()
+    assertTrue("An empty dataset's points should return None", emptyData.points == None)
+
+  }
+
+  @Test def noneName () {
+    val emptyData = new Data()
+    assertTrue("A dataset without a name should return None", emptyData.name == None)    
+  }
+    
+  @Test def noneVarNames () {
+    val emptyData = new Data()
+    assertTrue("A dataset without data points should return None", emptyData.varNames == None)    
+  }
+      
+  
+  // create a dataset with unequal number of values
+  
+  // create another dataset with unequal number of values
+  
+  // create 
 }
