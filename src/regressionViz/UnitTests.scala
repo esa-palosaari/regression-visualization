495e16ea3dd4ebb26d5acee691167b113f2e7cc4
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-03-04 14:54:46 +0200

    rewrite the Data class

--- src/regressionViz/UnitTests.scala
+++ src/regressionViz/UnitTests.scala
@@ -8,48 +8,59 @@ import org.junit.runner.RunWith
 class UnitTests {
   
   // Tests for an empty dataset
-  @Test def noneDataPoints () {
+  @Test def noneDataPoints () 
+  {
     val emptyData = new Data()
-    assertTrue("An empty dataset's points should return None", emptyData.points == None)
- 
+    assertTrue("An empty dataset's points should return None", emptyData.getPoints == None)
   }
 
-  @Test def noneDataName () {
+  @Test def noneDataName () 
+  {
     val emptyData = new Data()
-    assertTrue("A dataset without a name should return None", emptyData.name == None)    
+    assertTrue("A dataset without a name should return None", emptyData.getName == None)    
   }
     
-  @Test def noneDataVarNames () {
+  @Test def noneDataVarNames () 
+  {
     val emptyData = new Data()
-    assertTrue("A dataset without variable names should return None", emptyData.varNames == None)    
+    assertTrue("A dataset without variable names should return None", emptyData.getVarNames == None)    
   }
  
   // tests with something inside a dataset
-  @Test def someDataName () {
+  @Test def someDataName () 
+  {
     val someData = new Data(name = Some("dataset's name"))
-    assertTrue("A dataset with a name data points should return Some(name)", someData.name == Some("dataset's name"))    
+    assertTrue("A dataset with a name data points should return the same name in a wrapper", someData.getName == Some("dataset's name"))    
   }
   
-  @Test def someDataPointsExists () {
+  @Test def someDataPointsExists () 
+  {
     val someData = new Data(points = Some(Array(Array(1.23, 3.1343, 2), Array(0, 1, 2))))
-    assertTrue("A dataset with data some points should return points.isDefined == true", someData.points.isDefined == true)    
+    assertTrue("A dataset with data some points given should have them", someData.getPoints.isDefined)    
   }
   
-  @Test def someDataPoints () {
+  @Test def someDataPoints () 
+  {
     val someData = new Data(points = Some(Array(Array(1.23, 3.1343, 2), Array(0, 1, 2))))
 //    for{
 //      something <- someData.points
 //      array <- something
 //      point <- array
 //    } println(point)
-    assertTrue("A dataset with data some points should return the points", (
-        someData.points.get(0)(0) == 1.23 &&
-        someData.points.get(0)(1) == 3.1343 &&
-        someData.points.get(0)(2) == 2 &&
-        someData.points.get(1)(0) == 0 &&
-        someData.points.get(1)(1) == 1 &&
-        someData.points.get(1)(2) == 2) )    
+    assertTrue("A dataset with some data points should return the points", (
+        someData.getPoints.get(0)(0) == 1.23 &&
+        someData.getPoints.get(0)(1) == 3.1343 &&
+        someData.getPoints.get(0)(2) == 2 &&
+        someData.getPoints.get(1)(0) == 0 &&
+        someData.getPoints.get(1)(1) == 1 &&
+        someData.getPoints.get(1)(2) == 2) )    
   }  
+  
+  @Test def someDataVarNamesExist ()
+  {
+    val someData = new Data(points = Some(Array(Array(1.23, 3.1343, 2), Array(0, 1, 2))))
+    assertTrue("A dataset with some data points should have variable names", someData.getVarNames.isDefined)
+  }
  
   
   // create a dataset with unequal number of values  
