7b6767319a10cc4b5ef66e47d66aaefc07142d53
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-03-08 16:26:59 +0200

    OLS equation under work

--- src/regressionViz/OLSModel.scala
+++ src/regressionViz/OLSModel.scala
@@ -7,13 +7,36 @@ class OLSModel (data: Data) extends Model (data)
   {
     // listwise deletion for missing values
     if (fittedData.isEmpty) checkAndDeleteMissingRows
+    
     // check that there are enough rows to fit
+    require(fittedData.isDefined && 
+        fittedData.get.getPoints.isDefined &&
+        fittedData.get.getPoints.get.length > 1)
     
-    // calculate the normal equation
-    
-    // calculate the residuals
+    calculateNormalEquation
+    calculateResiduals
   }
   
+  protected def calculateNormalEquation =
+  {
+    val ata: Array[Array[Double]] = transpose_And_Multiply_Itself
+    val aty: Array[Double] = tranpose_And_Multiply_Outcome
+    val ataInverse: Array[Array[Double]] = invertMatrix
+    val estimate: Array[Double] = multiply_Matrix_And_Vector(ataInverse, aty)
+    equation = Some(estimate)
+  }
+  
+  def transpose_And_Multiply_Itself
+  def tranpose_And_Multiply_Outcome
+  def invertMatrix
+  def multiplyMatrix(Array[Array[Double]], Array[Array[Double]]) = 
+  {
+    
+  }
 
+  def calculateResiduals =
+  {
+    
+  }
   
 }
