144fc26a4a04aca453742dc127fca15e97cba8e2
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-03-26 19:31:20 +0200

    Engine.scala initial commit

---
+++ src/regressionViz/Engine.scala
@@ -0,0 +1,13 @@
+package regressionViz
+
+import scala.collection.mutable.ArrayBuffer
+
+class Engine {
+  
+  var data: ArrayBuffer[Data] = ArrayBuffer[Data]()
+  
+  def readData(fileName: String) 
+  {
+    
+  }
+}
