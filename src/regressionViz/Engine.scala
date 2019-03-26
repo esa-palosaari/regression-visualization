5913efd7c60224221bcd0b1f89b37c1a2e0690bf
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-03-26 19:47:01 +0200

    Engine.scala: added the basics from the plan

--- src/regressionViz/Engine.scala
+++ src/regressionViz/Engine.scala
@@ -5,8 +5,20 @@ import scala.collection.mutable.ArrayBuffer
 class Engine {
   
   var data: ArrayBuffer[Data] = ArrayBuffer[Data]()
+  var models: ArrayBuffer[Model] = ArrayBuffer[Model]()
+  var visuals: ArrayBuffer[Drawing] = ArrayBuffer[Drawing]()
   
   def readData(fileName: String) 
+  {
+    if (fileName.toLowerCase.endsWith(".csv"))
+    {
+      val reader = new CSVReader
+      data += reader.readFile(fileName).get
+    }
+    
+  }
+  
+  def saveImage(image: Drawing, fileName: String) 
   {
     
   }
