890bc813ef51071bcd14978c1e45c7b29f6f1572
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-04-04 16:34:09 +0300

    CLIApp.scala options added for usage

--- src/regressionViz/CLIApp.scala
+++ src/regressionViz/CLIApp.scala
@@ -1,8 +1,10 @@
 package regressionViz
 
-// example to follow: https://stackoverflow.com/questions/2315912/best-way-to-parse-command-line-parameters
+// examples followed: https://stackoverflow.com/questions/2315912/best-way-to-parse-command-line-parameters
 
 /*
+ * 	How to compile jar in Eclipse:
+ * 
  *  How to run the jar file:
  *  Install scala 2.12.3
  *  Install java 8 or later
@@ -10,7 +12,11 @@ package regressionViz
  */
 object CLIApp {
   val usage = """
-      Options: cliapp --d data.csv --o output.png
+      Usage: cliapp --d dataFileName.csv --o outputFileName.png 
+                      --modeltype [normal | log]
+                      --xmax [max_x-axis_value] --xmin [min_x-axis_value] 
+                      --ymax [max_y-axis_value] --ymin [min_y-axis_value]
+                      --pointcolor [COLORNAME] --curvecolor [COLORNAME]
     """
   val engine = new Engine
   type OptionMap = Map[Symbol, Any]
@@ -22,18 +28,41 @@ object CLIApp {
     
     var dataFilename = ""
     var imageFilename = ""
+    var modelType = ""
     
     args.sliding(2, 2).toList.collect 
     {
+      case Array("--modeltype", "normal") => modelType = "normal"
+      case Array("--modeltype", "log") => modelType = "log"
       case Array("--d", dname: String) => dataFilename = dname
       case Array("--o", iname: String) => imageFilename = iname
     }
+    
+    /*
+     * Käyttäjä pystyy lataamaan datajoukon valitsemastaan 
+     * tiedostosta, joka voi sisältää suurenkin määrän 
+     * (x, y)-koordinaattipareja.
+     * 
+     * TODO: Tuki vähintään kahdelle erilaiselle tiedostomuodolle.
+     */
     
     if(!dataFilename.equals("")) 
     {
       engine.readData(dataFilename)  
     } else println("Please give the name of the data file.")
     
+    /* 
+     * TODO: Toteuta vähintään yksi regressiomenetelmä yksinkertaisen 
+     * lineaariregression lisäksi. (Joko molemmat yhtälöt, tai 
+     * ensimmäinen yhtälö sekä itse valitsemasi muu menetelmä)
+     */
+    
+    /*
+     * Ohjelman tulee tehdä kuvaaja alkuperäisistä datapisteistä 
+     * sekä niihin sovitetusta mallista. Käyttäjä pystyy 
+     * säätämään kuvaajan asetuksia, esimerkiksi akselien 
+     * päätepisteitä.
+     */
     
     (dataFilename, imageFilename)
 
