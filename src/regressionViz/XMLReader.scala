1ea42d596bc78b84bc5827c638b37c14334d0a43
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-04-14 19:33:18 +0300

    XMLReader.scala returns None if variable names are wrong or have no data

--- src/regressionViz/XMLReader.scala
+++ src/regressionViz/XMLReader.scala
@@ -12,21 +12,32 @@ class XMLReader (var1Name: String, var2Name: String) extends DataReader
 {
   def readFile(filename: String): Option[Data] =
   {
-    if(filename.equals("")) return None
+    if( filename.equals("") || 
+        var1Name.equals("") || 
+        var2Name.equals("")) 
+      return None
+      
     var dataBuffer: ArrayBuffer[ArrayBuffer[Double]] = 
-      ArrayBuffer[ArrayBuffer[Double]]()    
+      ArrayBuffer[ArrayBuffer[Double]]() 
+      
     try
     {
       val xmlFile = XML.load(filename)
       val varOne = xmlFile \\ var1Name
       val varTwo = xmlFile \\ var2Name
       val oneBuffer = ArrayBuffer[Double]()
+      
       varOne.map(x => oneBuffer += x.text.toDouble)
       dataBuffer += oneBuffer
       val twoBuffer = ArrayBuffer[Double]()
       varTwo.map(x => twoBuffer += x.text.toDouble)
       dataBuffer += twoBuffer
-      var dataToArrays = dataBuffer.map(_.toArray).toArray
+      val dataToArrays = dataBuffer.map(_.toArray).toArray
+      
+      if( dataBuffer(0).length == 0 || 
+          dataBuffer(1).length == 0) 
+        return None // return None if there was no data
+        
       val data = new Data
       data.initializeDataset(newPoints = Some(dataToArrays), 
                             newVarNames = Some(ArrayBuffer(var1Name, var2Name)))
