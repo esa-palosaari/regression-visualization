41388212f223beb1623bbe9bccfb3dbd28ec4d33
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-04-14 13:41:56 +0300

    XMLReader.scala initial commit

---
+++ src/regressionViz/XMLReader.scala
@@ -0,0 +1,11 @@
+package regressionViz
+
+import scala.collection.mutable.ArrayBuffer
+
+class XMLReader extends DataReader
+{
+  def readFile(filename: String): Option[Data] =
+  {
+    None
+  }
+}
