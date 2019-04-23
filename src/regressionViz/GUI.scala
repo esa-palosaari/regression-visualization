8207f09de6bbee09631f610effb8d539b09d9190
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-04-23 20:18:01 +0300

    GUI.scala: outline for components added

--- src/regressionViz/GUI.scala
+++ src/regressionViz/GUI.scala
@@ -5,8 +5,40 @@ import java.awt.Color
 
 object GUI extends SimpleSwingApplication {
   
+  val engine = new Engine
+  
+  // the main window
   def top = new MainFrame
   {
-    
+    title = "Regression visualization"
+    contents = new BoxPanel(Orientation.Horizontal)
+    size = new Dimension(700, 700)
   }
+  
+  // message window
+  
+  // input box for data filename
+  
+  // input box for image filename
+  
+  // button for reading data
+  
+  // button for writing an image
+  
+  // choose model
+  
+  // window for the image
+  
+  // image size
+  
+  // vertical axis size
+  
+  // horizontal axis size
+  
+  // color values for data points
+  
+  // color values for the regression curve
+  
+  // input box for variable names
+  
 }
