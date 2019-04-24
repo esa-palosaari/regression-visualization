0c02d76e0845a7cb907c5f0c8c40332a7c1999b8
"Esa Palosaari" <esa.palosaari@gmail.com>
2019-04-24 10:58:01 +0300

    GUI: Button pressing and message area work

--- src/regressionViz/GUI.scala
+++ src/regressionViz/GUI.scala
@@ -2,20 +2,60 @@ package regressionViz
 
 import scala.swing._
 import java.awt.Color
+import scala.swing.BorderPanel.Position._
+import event._
 
 object GUI extends SimpleSwingApplication {
   
   val engine = new Engine
+
   
+
+
   // the main window
+  // examples: https://stackoverflow.com/questions/27621939/type-mismatch-error-when-adding-label-to-scala-swing-panel
+  // https://www.geeksforgeeks.org/java-swing-jpanel-examples/
   def top = new MainFrame
   {
     title = "Regression visualization"
-    contents = new BoxPanel(Orientation.Horizontal)
-    size = new Dimension(700, 700)
+    
+    // error/success messages
+    val messagePanel = new TextArea
+    {
+      text = "Program started."
+      background = Color.green
+    }
+    
+    // trying out a button
+    val button = new Button
+    {
+      text = "Try me!"
+      foreground = Color.blue
+      background = Color.red
+      borderPainted = true 
+      enabled = true
+    }
+      
+    contents = new BorderPanel
+    {
+      layout(button) = East
+      layout(messagePanel) = South
+    }
+    
+    val dim = new Dimension(700, 700)
+    minimumSize = dim
+    preferredSize = dim
+    foreground = Color.WHITE
+    
+    listenTo(button)
+    
+    reactions += 
+    {
+      case ButtonClicked(component) if component == button =>
+        messagePanel.text_=("Button press successful!")
+        messagePanel.background = Color.cyan
+    }
   }
-  
-  // message window
   
   // input box for data filename
   
